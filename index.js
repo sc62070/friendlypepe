c039dce897788dfc4a6f049e48a2e40a4b8c93cc
Hyuk Jin Choi <62084630+sc62070@users.noreply.github.com>
2024-01-11 10:50:33 +0900

    cool

---
+++ index.js
@@ -0,0 +1,6 @@
+setInterval(() =>{
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+	a.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Feels_good_man.jpg/200px-Feels_good_man.jpg'
+})
+}, 500)
